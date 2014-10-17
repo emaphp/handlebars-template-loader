7eb2a2a77765d0a8752e7ed764374e1d06eb627e
emaphp <emmanuel.antico@gmail.com>
2014-10-17 17:17:12 -0300

    fixed: precompiling templates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Handlebars = require('handlebars');
 module.exports = function(content) {
     this.cacheable && this.cacheable();
     var callback = this.async();
-    var fn = Handlebars.compile(content);
-    callback(null, "module.exports = " + fn.source + ";");
+    var fn = Handlebars.precompile(content);
+    callback(null, "var Handlebars = require('" + require.resolve("handlebars/runtime") + "');\n" +
+    "module.exports = (Handlebars[\"default\"] || Handlebars).template(" + fn + ");");
 }
