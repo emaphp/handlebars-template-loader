95ddc5b4963f500c2b28d0eebcedbdd4644f9694
emaphp <emmanuel.antico@gmail.com>
2014-10-18 19:36:38 -0300

    fix: use require.resolve to reference used handlebars lib

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(content) {
     this.cacheable && this.cacheable();
     var callback = this.async();
     var fn = Handlebars.precompile(content);
-    callback(null, "var Handlebars = require('handlebars');\n" +
+    callback(null, "var Handlebars = require('" + require.resolve('handlebars') + "');\n" +
     "module.exports = (Handlebars[\"default\"] || Handlebars).template(" + fn + ");");
 };
 
