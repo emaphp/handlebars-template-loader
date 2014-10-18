c1248deebe6618dd3f96da03e11745996bd433fb
emaphp <emmanuel.antico@gmail.com>
2014-10-18 19:12:15 -0300

    wrapping handlebars in module.exports

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = function(content) {
     this.cacheable && this.cacheable();
     var callback = this.async();
     var fn = Handlebars.precompile(content);
-    callback(null, "var Handlebars = require('" + require.resolve("handlebars/runtime") + "');\n" +
+    callback(null, "var Handlebars = require('handlebars');\n" +
     "module.exports = (Handlebars[\"default\"] || Handlebars).template(" + fn + ");");
-}
+};
+
+module.exports.Handlebars = Handlebars;
