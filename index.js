d6fa4c407b7981aa56d3e215c472b0df2f31f1fb
emaphp <emmanuel.antico@gmail.com>
2015-01-22 16:21:02 -0300

    handlebars as a peer dependency

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 var Handlebars = require('handlebars');
-var loaderUtils = require('loader-utils');
-var path = require('path');
-var fs = require('fs');
 
-function resolveContent(content, root) {
-    var includeRegex = /<!--include\s+([\/\w\.]*?[\w]+\.[\w]+)-->/g;
-    var matches = includeRegex.exec(content);
+module.exports = function() {
+	var loaderUtils = require('loader-utils');
+	var path = require('path');
+	var fs = require('fs');
+	
+	var readFile = function(filepath, root) {
+		var self = readFile;
+		self.buffer = self.buffer || {};
 
-    while (matches != null) {
-        var file = loaderUtils.urlToRequest(matches[1]);
-        var basename = path.basename(file);
-        var dirname = path.join(root, path.dirname(file));
-        var rawContent = readFile(basename, dirname);
-        content = content.replace(matches[0], rawContent);
-        matches = includeRegex.exec(content);
-    }
+		if (filepath in self.buffer)
+			return self.buffer[filepath];
+		
+		var content = readContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);    
+		self.buffer[filepath] = content;
+		return self.buffer[filepath];
+	};
+	
+	var readContent = function(content, root) {
+		var includeRegex = /<!--include\s+([\/\w\.]*?[\w]+\.[\w]+)-->/g;
+		var matches = includeRegex.exec(content);
 
-    return content;
-}
+		while (matches != null) {
+			var file = loaderUtils.urlToRequest(matches[1]);
+			var rawContent = readFile(path.basename(file), path.join(root, path.dirname(file)));
+			content = content.replace(matches[0], rawContent);
+			matches = includeRegex.exec(content);
+		}
 
-function readFile(filepath, root) {
-    var self = readFile;
+		return content;
+	};
 
-    if (typeof(self.buffer) == "undefined") {
-        self.buffer = {};
-    }
-
-    if (filepath in self.buffer) {
-        return self.buffer[filepath];
-    }
-    
-    var content = resolveContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);    
-    self.buffer[filepath] = content;
-    return self.buffer[filepath];
-}
-
-module.exports = function(content) {
-    this.cacheable && this.cacheable();
-    var callback = this.async();
-    content = resolveContent(content, this.context);
-    var fn = Handlebars.precompile(content);
-    callback(null, "var Handlebars = require('" + require.resolve('handlebars') + "');\n" +
-    "module.exports = (Handlebars[\"default\"] || Handlebars).template(" + fn + ");");
-};
+	return function(content) {
+		this.cacheable && this.cacheable();
+		var callback = this.async();
+		content = readContent(content, this.context);
+		var fn = Handlebars.precompile(content);
+		callback(null, "var Handlebars = require('" + require.resolve('handlebars') + "');\n" +
+		"module.exports = (Handlebars[\"default\"] || Handlebars).template(" + fn + ");");
+	};
+}();
 
 module.exports.Handlebars = Handlebars;
